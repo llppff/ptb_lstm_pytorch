6909d9950655f38511f0c4ffcde71b6dfee83349
llppff <llppff15022391929@163.com>
2020-03-07 10:57:03 +0800

    修改train

--- main.py
+++ main.py
@@ -11,67 +11,6 @@ import model
 import torch.optim as optim
 from adabound import AdaBound
 
-# 建立解析对象
-parser = argparse.ArgumentParser(description='LSTM PTB Language Model')
-#增加属性
-parser.add_argument('--data', type=str, default='ptb_lstm_pytorch/data/ptb',
-                    help='location of the data corpus')
-parser.add_argument('--model', type=str, default='LSTM',
-                    help='type of recurrent net (LSTM, GRU)')
-parser.add_argument('--embedding_size', type=int, default=1500,
-                    help='size of word embeddings')
-parser.add_argument('--num_hid_unit', type=int, default=1500,
-                    help='number of hidden units per layer')
-parser.add_argument('--nlayers', type=int, default=2,
-                    help='number of layers')
-parser.add_argument('--lr', type=float, default=22,
-                    help='initial learning rate')
-parser.add_argument('--clip', type=float, default=0.25,
-                    help='gradient clipping')
-parser.add_argument('--epochs', type=int, default=40,
-                    help='upper epoch limit')
-parser.add_argument('--batch_size', type=int, default=20, metavar='N',
-                    help='batch size')
-parser.add_argument('--bptt', type=int, default=35,
-                    help='sequence length')
-parser.add_argument('--dropout', type=float, default=0.65,
-                    help='dropout applied to layers (0 = no dropout)')
-parser.add_argument('--tied', action='store_true',
-                    help='tie the word embedding and softmax weights')
-parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed')
-parser.add_argument('--seed_gpu', type=int, default=1111,
-                    help='random seed')
-parser.add_argument('--cuda', action='store_true',
-                    help='use CUDA')
-parser.add_argument('--log-interval', type=int, default=200, metavar='N',
-                    help='report interval')
-parser.add_argument('--save', type=str, default='model.pt',
-                    help='path to save the final model')
-
-parser.add_argument('--optim', default='sgd', type=str, help='optimizer',
-                    choices=['sgd', 'adagrad', 'adam', 'amsgrad', 'adabound', 'amsbound'])
-parser.add_argument('--momentum', default=0.9, type=float, help='momentum term')
-parser.add_argument('--beta1', default=0.9, type=float, help='Adam coefficients beta_1')
-parser.add_argument('--beta2', default=0.999, type=float, help='Adam coefficients beta_2')
-parser.add_argument('--final_lr', default=0.1, type=float,
-                    help='final learning rate of AdaBound')
-parser.add_argument('--gamma', default=1e-3, type=float,)
-parser.add_argument('--weight_decay', default=5e-4, type=float,
-                    help='weight decay for optimizers')
-
-# 属性给与args实例： 把parser中设置的所有"add_argument"给返回到args子类实例当中， 那么parser中增加的属性内容都会在args实例中，使用即可。
-args = parser.parse_args()
-
-
-torch.manual_seed(args.seed)#为CPU设置种子用于生成随机数，以使得生成随机数的结果是确定的
-torch.cuda.manual_seed(args.seed_gpu)#为当前GPU设置随机种子
-
-device = torch.device("cuda" if args.cuda else "cpu")
-
-#将数据集设置好，见data.py中
-corpus = data.Corpus(args.data)
-
 def create_optimizer(args, model_params):
     if args.optim == 'sgd':
         return optim.SGD(model_params, args.lr, momentum=args.momentum,
@@ -101,21 +40,46 @@ def batchify(data, bsz):
     data = data.view(bsz, -1).t().contiguous()#在调用contiguous()之后，PyTorch会开辟一块新的内存空间存放变换之后的数据,而view是新数据与原数据共享一块内存
     return data.to(device)
 
-eval_batch_size = 10
-train_data = batchify(corpus.train, args.batch_size)
-val_data = batchify(corpus.valid, eval_batch_size)
-test_data = batchify(corpus.test, eval_batch_size)
-
-ntokens = len(corpus.dictionary)
-model = model.RNNModel(args.model, ntokens, args.embedding_size, args.num_hid_unit, args.nlayers, args.dropout, args.tied).to(device)
-
-criterion = nn.CrossEntropyLoss()
-
 def get_batch(source, i):
     seq_len = min(args.bptt, len(source) - 1 - i)
     data = source[i:i+seq_len]
     target = source[i+1:i+1+seq_len].view(-1)
     return data, target
+
+def train():
+    model.train()
+    total_loss = 0.
+    start_time = time.time()
+    ntokens = len(corpus.dictionary)
+    hidden = model.init_hidden(args.batch_size)
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
+        print("batch : " + str(batch))
+        data, targets = get_batch(train_data, i)
+        hidden = repackage_hidden(hidden)
+
+        optimizer = create_optimizer(args, model.parameters())
+        optimizer.zero_grad()
+        output, hidden = model(data, hidden)
+        loss = criterion(output.view(-1, ntokens), targets)
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+        for p in model.parameters():
+            p.data.add_(-lr, p.grad.data)
+
+        total_loss += loss.item()
+
+        if batch % args.log_interval == 0 and batch > 0:
+            train_loss = total_loss / args.log_interval
+        #     elapsed = time.time() - start_time
+        #     # logging.info('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+        #     #         'loss {:5.2f} | ppl {:8.2f}'.format(
+        #     #     epoch, batch, len(train_data) // args.bptt, lr,
+        #     #     elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+        #     print("train_loss:" + str(train_loss))
+            logging.info('train_loss:{:5.2f}'.format(train_loss))
+            total_loss = 0
+        #     start_time = time.time()
 
 def evaluate(data_source):
     model.eval()
@@ -161,42 +125,78 @@ def initLogging(logFilename):
     console.setFormatter(formatter)
     logging.getLogger('').addHandler(console)
 
+
+# 建立解析对象
+parser = argparse.ArgumentParser(description='LSTM PTB Language Model')
+#增加属性
+parser.add_argument('--data', type=str, default='ptb_lstm_pytorch/data/ptb',
+                    help='location of the data corpus')
+parser.add_argument('--model', type=str, default='LSTM',
+                    help='type of recurrent net (LSTM, GRU)')
+parser.add_argument('--embedding_size', type=int, default=1500,
+                    help='size of word embeddings')
+parser.add_argument('--num_hid_unit', type=int, default=1500,
+                    help='number of hidden units per layer')
+parser.add_argument('--nlayers', type=int, default=2,
+                    help='number of layers')
+parser.add_argument('--lr', type=float, default=22,
+                    help='initial learning rate')
+parser.add_argument('--clip', type=float, default=0.25,
+                    help='gradient clipping')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='upper epoch limit')
+parser.add_argument('--batch_size', type=int, default=20, metavar='N',
+                    help='batch size')
+parser.add_argument('--bptt', type=int, default=35,
+                    help='sequence length')
+parser.add_argument('--dropout', type=float, default=0.65,
+                    help='dropout applied to layers (0 = no dropout)')
+parser.add_argument('--tied', action='store_true',
+                    help='tie the word embedding and softmax weights')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+parser.add_argument('--seed_gpu', type=int, default=1111,
+                    help='random seed')
+parser.add_argument('--cuda', action='store_true',
+                    help='use CUDA')
+parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                    help='report interval')
+parser.add_argument('--save', type=str, default='model.pt',
+                    help='path to save the final model')
+
+parser.add_argument('--optim', default='sgd', type=str, help='optimizer',
+                    choices=['sgd', 'adagrad', 'adam', 'amsgrad', 'adabound', 'amsbound'])
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum term')
+parser.add_argument('--beta1', default=0.9, type=float, help='Adam coefficients beta_1')
+parser.add_argument('--beta2', default=0.999, type=float, help='Adam coefficients beta_2')
+parser.add_argument('--final_lr', default=0.1, type=float,
+                    help='final learning rate of AdaBound')
+parser.add_argument('--gamma', default=1e-3, type=float,)
+parser.add_argument('--weight_decay', default=5e-4, type=float,
+                    help='weight decay for optimizers')
+
+# 属性给与args实例： 把parser中设置的所有"add_argument"给返回到args子类实例当中， 那么parser中增加的属性内容都会在args实例中，使用即可。
+args = parser.parse_args()
+
+torch.manual_seed(args.seed)#为CPU设置种子用于生成随机数，以使得生成随机数的结果是确定的
+torch.cuda.manual_seed(args.seed_gpu)#为当前GPU设置随机种子
+
+device = torch.device("cuda" if args.cuda else "cpu")
+
+#将数据集设置好，见data.py中
+corpus = data.Corpus(args.data)
+
+eval_batch_size = 10
+train_data = batchify(corpus.train, args.batch_size)
+val_data = batchify(corpus.valid, eval_batch_size)
+test_data = batchify(corpus.test, eval_batch_size)
+
+ntokens = len(corpus.dictionary)
+model = model.RNNModel(args.model, ntokens, args.embedding_size, args.num_hid_unit, args.nlayers, args.dropout, args.tied).to(device)
+
+criterion = nn.CrossEntropyLoss()
+
 initLogging('test.log')
-
-def train():
-    model.train()
-    total_loss = 0.
-    start_time = time.time()
-    ntokens = len(corpus.dictionary)
-    hidden = model.init_hidden(args.batch_size)
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
-        data, targets = get_batch(train_data, i)
-        hidden = repackage_hidden(hidden)
-
-        optimizer = create_optimizer(args, model.parameters())
-        optimizer.zero_grad()
-        output, hidden = model(data, hidden)
-        loss = criterion(output.view(-1, ntokens), targets)
-        loss.backward()
-
-        torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
-        for p in model.parameters():
-            p.data.add_(-lr, p.grad.data)
-
-        total_loss += loss.item()
-
-        if batch % args.log_interval == 0 and batch > 0:
-            train_loss = total_loss / args.log_interval
-        #     elapsed = time.time() - start_time
-        #     # logging.info('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-        #     #         'loss {:5.2f} | ppl {:8.2f}'.format(
-        #     #     epoch, batch, len(train_data) // args.bptt, lr,
-        #     #     elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
-            print("train_loss:" + str(train_loss))
-            logging.info('train loss:{:5.2f}'.format(train_loss))
-            total_loss = 0
-        #     start_time = time.time()
-
 
 lr = args.lr
 # best_val_loss = None
@@ -211,8 +211,8 @@ try:
         # logging.info('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
         #         'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
         #                                    val_loss, math.exp(val_loss)))
-        print("valid_loss:" + str(val_loss))
-        logging.info('valid loss:{:5.2f}'.format(val_loss))
+        # print("valid_loss:" + str(val_loss))
+        logging.info('valid_loss:{:5.2f}'.format(val_loss))
         # logging.info('-' * 89)
         # if not best_val_loss or val_loss < best_val_loss:
         #     with open(args.save, 'wb') as f:
@@ -230,11 +230,11 @@ except KeyboardInterrupt:
 #     model.rnn.flatten_parameters()
 
 test_loss = evaluate(test_data)
-print("test_loss:" + str(test_loss))
-logging.info('=' * 89)
+# print("test_loss:" + str(test_loss))
+# logging.info('=' * 89)
 # logging.info('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
 #     test_loss, math.exp(test_loss)))
-logging.info('test loss {:5.2f}'.format(test_loss))
-logging.info('=' * 89)
+logging.info('test_loss {:5.2f}'.format(test_loss))
+# logging.info('=' * 89)
 
 
